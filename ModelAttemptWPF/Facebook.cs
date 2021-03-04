23f773e7b66eb5bd5ca409877dc0bf25f5a674ea
Se-Si <sean.sirur@gmail.com>
2021-03-04 04:34:42 +0000

    Updated network generator (python script + CSV parsing)

--- ModelAttemptWPF/Facebook.cs
+++ ModelAttemptWPF/Facebook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 
 namespace ModelAttemptWPF
@@ -9,7 +10,6 @@ namespace ModelAttemptWPF
         {
 
         }
-
         public new void CreateRandomMutualFollows(Account account, int nConnections)
         {
             List<int> connectionIDS = new List<int>();
@@ -32,20 +32,38 @@ namespace ModelAttemptWPF
             }
         }
 
+        public void generateSmallWorldNetwork(string[] args)
+        {
+            string networkGeneratorPath = @"..\..\..\FacebookUK\network_generator.py";
+            string smallWorldNetworkPath = @"..\..\..\FacebookUK\small_world_network.csv.py";
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "my/full/path/to/python.exe";
+            start.Arguments = string.Format("{0} {1}", cmd, args);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using(Process process = Process.Start(start))
+            {
+                using(StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.Write(result);
+                }
+            }
+        }
         public new void CreateMutualFollowsFromGraph(string filePath)
         {
             List<string[]> connections = LoadCsvFile(filePath);
             foreach (string[] connection in connections)
             {
-                // string[0] is the key and isn't necesary
-                int followerID = Convert.ToInt16(connection[1]);
-                int followeeID = Convert.ToInt16(connection[2]);
+                int followerID = Convert.ToInt16(connection[0]);
+                int followeeID = Convert.ToInt16(connection[1]);
+                // TODO: should this be bi-directionally symmetric by default? We can modify this at some point.
                 this.Follow(accountList[followeeID], accountList[followerID]);
                 this.Follow(accountList[followerID], accountList[followeeID]);
             }
         }
-        public new void CreateFollowsBasedOnPersonality(int defaultFollows)
-        //Change this to take a default number of follows
+        public void CreateFollowsBasedOnPersonality(int defaultFollows)
+        // TODO: Change this to take a default number of follows
         {
             foreach (Account account in this.accountList)
             {
