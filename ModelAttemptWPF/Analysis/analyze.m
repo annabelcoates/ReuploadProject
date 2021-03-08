225e0e7093fa8f9f1547b4dca6b33c9ea8ab8b34
Se-Si <15632209+Se-Si@users.noreply.github.com>
2021-03-08 23:28:15 +0000

    Plot Update: Functional Histogram and Scatter

--- ModelAttemptWPF/Analysis/analyze.m
+++ ModelAttemptWPF/Analysis/analyze.m
@@ -41,39 +41,38 @@ end
 for varIdx = 1:varParamVals_len
     for i=1:nRuns
         fileName = resultsPaths{i, varIdx};
-        disp(fileName)
-        varParam = [varParamVals{varIdx} int2str(i)];
         nSharesPop=csvread(fileName,1,1);
         nFollowers(i,:)=nSharesPop(:,1);
-        o(i,:)=nSharesPop(:,2);
-        c(i,:)=nSharesPop(:,3);
-        e(i,:)=nSharesPop(:,4);
-        a(i,:)=nSharesPop(:,5);
-        n(i,:)=nSharesPop(:,6);
+        opn(i,:)=nSharesPop(:,2);
+        con(i,:)=nSharesPop(:,3);
+        ext(i,:)=nSharesPop(:,4);
+        agr(i,:)=nSharesPop(:,5);
+        nrt(i,:)=nSharesPop(:,6);
         OL(i,:)=nSharesPop(:,7);
         trueShares(i,:)=nSharesPop(:,8);
         fakeShares(i,:)=nSharesPop(:,9);
         ratio(i,:)=nSharesPop(:,10);
-        freqUse(i,:)=nSharesPop(:,11);
-        sessionLength(i,:)=20.*(nSharesPop(:,12));
+        frqUse(i,:)=nSharesPop(:,11);
+        sesLen(i,:)=20.*(nSharesPop(:,12));
         shareFreq(i,:)=nSharesPop(:,13);
     end
-    runParamsStruct.varParam.nFollowers=reshape(nFollowers,[1,nRuns*population]);
-    runParamsStruct.varParam.o=reshape(o,[1,nRuns*population]);
-    runParamsStruct.varParam.c=reshape(c,[1,nRuns*population]);
-    runParamsStruct.varParam.e=reshape(e,[1,nRuns*population]);
-    runParamsStruct.varParam.a=reshape(a,[1,nRuns*population]);
-    runParamsStruct.varParam.n=reshape(n,[1,nRuns*population]);
-    runParamsStruct.varParam.OL=reshape(OL,[1,nRuns.*population]);
-    runParamsStruct.varParam.trueShares=reshape(trueShares,[1,nRuns*population]);
-    runParamsStruct.varParam.fakeShares=reshape(fakeShares,[1,nRuns*population]);
-    runParamsStruct.varParam.ratio=reshape(ratio,[1,nRuns*population]);
-    runParamsStruct.varParam.freqUse=reshape(freqUse,[1,nRuns*population]);
-    runParamsStruct.varParam.sessionLength=reshape(sessionLength,[1,nRuns*population]);
+    varParam = erase(varParamVals{varIdx}, '_');
+    runParamsStruct.(varParam).nFollowers=reshape(nFollowers,[1,nRuns*population]);
+    runParamsStruct.(varParam).opn=reshape(opn,[1,nRuns*population]);
+    runParamsStruct.(varParam).con=reshape(con,[1,nRuns*population]);
+    runParamsStruct.(varParam).ext=reshape(ext,[1,nRuns*population]);
+    runParamsStruct.(varParam).agr=reshape(agr,[1,nRuns*population]);
+    runParamsStruct.(varParam).nrt=reshape(nrt,[1,nRuns*population]);
+    runParamsStruct.(varParam).OL=reshape(OL,[1,nRuns.*population]);
+    runParamsStruct.(varParam).trueShares=reshape(trueShares,[1,nRuns*population]);
+    runParamsStruct.(varParam).fakeShares=reshape(fakeShares,[1,nRuns*population]);
+    runParamsStruct.(varParam).ratio=reshape(ratio,[1,nRuns*population]);
+    runParamsStruct.(varParam).frqUse=reshape(frqUse,[1,nRuns*population]);
+    runParamsStruct.(varParam).sesLen=reshape(sesLen,[1,nRuns*population]);
     % // TODO 
     % ? Should this be a ceiling function
-    runParamsStruct.varParam.roundSL=ceil(runParamsStruct.sessionLength);
-    runParamsStruct.varParam.shareFreq=reshape(shareFreq,[1,nRuns*population]);
+    runParamsStruct.(varParam).roundSL=ceil(runParamsStruct.(varParam).sesLen);
+    runParamsStruct.(varParam).shareFreq=reshape(shareFreq,[1,nRuns*population]);
 end
 %%
 
@@ -95,14 +94,50 @@ end
 
 %%
 
-
+folderName = datestr(datetime('now'));
+folderName = strrep(folderName,':','-');
+folderName = [folderName ' Analysis'];
+mkdir(folderName);
 
 %%
 
-function []=myPlot(x,y,xtitle,ytitle)
-    
-    figure()
-    scatter(x,y,3)
-    xlabel(xtitle)
-    ylabel(ytitle)
+for idx = 1:plotParamsString_len
+    disp(idx)
+    myPlot(plotParams{idx, 1}, plotParams{idx, 2}, runParamsStruct, varParamVals, nRuns, folderName)
+end
+%%
+
+function []=myPlot(x, y, runParamsStruct, varParamVals, nRuns, folderName)
+    disp(x)
+    disp(y)
+    disp(isempty(y))
+    varParamVals_len = size(varParamVals);
+    varParamVals_len = varParamVals_len(2);
+    for varIdx = 1:varParamVals_len
+        varParam = erase(varParamVals{varIdx}, '_');
+        if isempty(y)
+            figure();
+            histogram(...
+                runParamsStruct.(varParam).(x),...
+                50,'Normalization','pdf');
+            xlabel(x);
+            ylabel('PDF');
+            figName = [varParam 'Histogram plot of ' x];
+            savePath = fullfile(folderName, [figName '.png']);
+            title(figName);
+            saveas(gcf, [savePath '.png']);
+        else
+            figure();
+            scatter(...
+                runParamsStruct.(varParam).(x),...
+                runParamsStruct.(varParam).(y),...
+                3);
+            xlabel(x);
+            ylabel(y);
+            figName = [varParam 'Scatter plot of ' x ' against ' y];
+            savePath = fullfile(folderName, [figName '.png']);
+            title(figName);
+            saveas(gcf, [savePath '.png']);
+        end
+    end
 end
